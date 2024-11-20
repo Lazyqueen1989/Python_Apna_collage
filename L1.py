bb75a4396c68b4cfad1f9fcd864ee594bf1ed1f6
kulsum_syed_18 <125910160+Lazyqueen1989@users.noreply.github.com>
2024-11-20 15:43:17 +0530

    Add files via upload

---
+++ L1.py
@@ -0,0 +1,11 @@
+# what is python?
+# simple and easy coding language, .free and open source,
+# # .high level og language, .portable, .developed by Guido van rossum
+print("hello world")
+print("kulsum")
+# python Character set
+# letter- A to Z, a to z
+# Digits- 0 to 9
+# special symbols- + _ * / etc.
+# whitespace- Blank Space, tab, carriage return, newline, formfeed
+# other characters- Python can process all ASCLL and Unicode characters as part of data or literal
